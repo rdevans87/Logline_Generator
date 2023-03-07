073d4f435351f51ee7616b4615c36d4be6161bdc
Ryan Evans <rdevans87@gmail.com>
2023-03-06 20:37:21 -0500

    added buttons to top nav for save, download, start over

--- assets/js/logline.js
+++ assets/js/logline.js
@@ -31,7 +31,35 @@
         logline = `A ${inputValues.leadCharacter} ${inputValues.centralConflict} ${inputValues.opposingForce} ${inputValues.mainGoal}.`;
         break;
     }
-  
+
+    // function saveStory() {
+      // Get the input values
+    //   const incitingIncident = document.getElementById('incitingIncident').value;
+    //   const protagonist = document.getElementById('protagonist').value;
+    //   const mainGoal = document.getElementById('mainGoal').value;
+    //   const antagonist = document.getElementById('antagonist').value;
+    //   const obstacle = document.getElementById('obstacle').value;
+    //   const painPoint = document.getElementById('painPoint').value;
+    //   const setting = document.getElementById('setting').value;
+    //   const forcedAction = document.getElementById('forcedAction').value;
+    //   const badOutcome = document.getElementById('badOutcome').value;
+    
+    //   // Save the story object to localStorage
+    //   const story = {
+    //     incitingIncident,
+    //     protagonist,
+    //     mainGoal,
+    //     antagonist,
+    //     obstacle,
+    //     painPoint,
+    //     setting,
+    //     forcedAction,
+    //     badOutcome
+    //   };
+    //   localStorage.setItem('story', JSON.stringify(story));
+    // // }
+
+   
     // Create a new logline container
     const loglineContainer = document.createElement('div');
     loglineContainer.classList.add('logline-container');
@@ -50,4 +78,72 @@
   }
   
 
- 
+  function downloadLoglines() {
+    // Get all the logline text elements on the page
+    const loglines = document.querySelectorAll('.logline');
+  
+    // Create a new text file
+    const file = new Blob([loglines.map(logline => logline.innerText).join('\n')], {type: 'text/plain'});
+  
+    // Create a URL for the file
+    const url = URL.createObjectURL(file);
+  
+    // Create a link to download the file
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'loglines.txt';
+  
+    // Click the link to start the download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+  
+  document.getElementById('download-btn').addEventListener('click', downloadLoglines);
+
+  
+function saveResults() {
+  // Get all the input values
+  const inputValues = {
+    'when-a': {},
+    'after-a': {},
+    'a': {},
+  };
+  const inputFields = document.querySelectorAll('#logline-type option:checked ~ .logline-inputs input');
+  for (const inputField of inputFields) {
+    inputValues[inputField.closest('.logline-inputs').id][inputField.name] = inputField.value;
+  }
+
+  // Get all the generated loglines
+  const loglineElements = document.querySelectorAll('.logline');
+  const loglines = [];
+  for (const loglineElement of loglineElements) {
+    loglines.push(loglineElement.textContent);
+  }
+
+  // Store the input values and loglines in local storage
+  const data = {
+    'inputValues': inputValues,
+    'loglines': loglines,
+  };
+  localStorage.setItem('loglineData', JSON.stringify(data));
+}
+
+
+function startOver() {
+  // Clear all input fields
+  const inputFields = document.querySelectorAll(`input[type="text"]`);
+  for (const inputField of inputFields) {
+    inputField.value = '';
+  }
+
+  // Clear all generated loglines
+  const loglineContainer = document.getElementById('logline');
+  loglineContainer.innerHTML = '';
+
+  // Clear all stored data in local storage
+  localStorage.clear();
+
+  // Refresh the page
+  location.reload();
+}
